c2af9d2e7de7b51c1eb069a3a8620a4e9123ca2b
NikitaChernikov <nikitachernikov20006@gmail.com>
2023-06-17 09:33:00 +0300

    Car scene added.
    Need improvements?

--- Assets/2. Car/Scripts/CarInputHandler.cs
+++ Assets/2. Car/Scripts/CarInputHandler.cs
@@ -1,7 +1,39 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CarInputHandler : MonoBehaviour
 {
+    private int _verticalInput;
+    private int _horizontalInput;
+    private bool _isBreak;
 
+    public void SetVerticalInput(int moveInput)
+    {
+        _verticalInput = moveInput;
+    }
+
+    public void SetHorizontalInput(int moveInput)
+    {
+        _horizontalInput = moveInput;
+    }
+
+    public void SetBreakState(bool isBreak)
+    {
+        _isBreak = isBreak;
+    }
+
+    public int GetVerticalInput()
+    {
+        return _verticalInput;
+    }
+
+    public int GetHorizontalInput()
+    {
+        return _horizontalInput;
+    }
+
+    public bool GetBreakState()
+    {
+        return _isBreak;
+    }
+    
 }
--- Assets/2. Car/Scripts/CarMovement.cs
+++ Assets/2. Car/Scripts/CarMovement.cs
@@ -1,15 +1,34 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(CarInputHandler))]
 public class CarMovement : MonoBehaviour
 {
     [SerializeField] private AxleInfo[] axleInfos;
     [SerializeField] private float _maxMotorTorque;
     [SerializeField] private float _maxSteeringAngle;
+    [SerializeField] private float _breakForce = 10000;
+
+    [SerializeField] private Vector3 _carCenterOfMass = new Vector3(0, 0, 0);
+
+    private CarInputHandler _carInput;
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _carInput = GetComponent<CarInputHandler>();
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        _rb.centerOfMass = _carCenterOfMass;
+    }
 
     private void FixedUpdate()
     {
-        float motor = _maxMotorTorque * joystick.Vertical; //ускорение
-        float steering = _maxSteeringAngle * joystick.Horizontal; //угол поворота
+        float motor = _maxMotorTorque * _carInput.GetVerticalInput(); 
+        float steering = _maxSteeringAngle * _carInput.GetHorizontalInput(); 
         foreach (AxleInfo axleInfo in axleInfos)
         {
             if (axleInfo.isSteering)
@@ -22,6 +41,33 @@ public class CarMovement : MonoBehaviour
                 axleInfo.LeftWheel.motorTorque = motor; //здесь в зависимости от локальных координат выбираем - или +
                 axleInfo.RightWheel.motorTorque = motor;  //здесь в зависимости от локальных координат выбираем - или +              
             }
+            if (_carInput.GetBreakState())
+            {
+                axleInfo.LeftWheel.brakeTorque = _breakForce;
+                axleInfo.RightWheel.brakeTorque = _breakForce;
+            }
+            else
+            {
+                axleInfo.LeftWheel.brakeTorque = 0;
+                axleInfo.RightWheel.brakeTorque = 0;
+            }
+            ChangeWheelsVisual(axleInfo.LeftWheel);
+            ChangeWheelsVisual(axleInfo.RightWheel);
         }
+    }
+
+    private void ChangeWheelsVisual(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+        Transform visualWheel = collider.transform.GetChild(0);
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+        visualWheel.transform.position = Vector3.Lerp(visualWheel.transform.position,position, 0.1f);
+        visualWheel.transform.rotation = Quaternion.Lerp(visualWheel.transform.rotation, rotation, 0.1f);
+
     }
 }
