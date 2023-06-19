c27ea30a35cdb6882f068ac071ea9f50bc0b1694
NikitaChernikov <nikitachernikov20006@gmail.com>
2023-06-19 11:37:36 +0300

    Animation Rigging added

--- Assets/3. Shooter/Scripts/ThirdPersonCamera.cs
+++ Assets/3. Shooter/Scripts/ThirdPersonCamera.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    [SerializeField] private GameObject _player;
+    [SerializeField] private Transform _player;
     [SerializeField] [Range(0.01f, 0.5f)] private float _mouseSense = 1;
     [SerializeField] [Range(-20, -10)] private int _lookUpRestriction = -15;
     [SerializeField] [Range(15, 25)] private int _lookDownRestriction = 20;
+    [SerializeField] Transform _aimRigTarget;
+    private Vector3 _aimRigInitialOffset;
+
+    private void Start()
+    {
+        _aimRigInitialOffset = _aimRigTarget.position - transform.position;
+    }
 
     void Update()
     {
@@ -17,7 +24,7 @@ public class ThirdPersonCamera : MonoBehaviour
                 float rotateY = touch.deltaPosition.y * _mouseSense;
 
                 Vector3 rotCamera = transform.rotation.eulerAngles;
-                Vector3 rotPlayer = _player.transform.rotation.eulerAngles;
+                Vector3 rotPlayer = _player.rotation.eulerAngles;
 
                 rotCamera.x -= rotateY;
                 rotCamera.x = (rotCamera.x > 180) ? rotCamera.x - 360 : rotCamera.x;
@@ -26,7 +33,17 @@ public class ThirdPersonCamera : MonoBehaviour
                 rotPlayer.y += rotateX;
 
                 transform.rotation = Quaternion.Euler(rotCamera);
-                _player.transform.rotation = Quaternion.Euler(rotPlayer);
+                _player.rotation = Quaternion.Euler(rotPlayer);
+
+                if (_aimRigTarget)
+                {
+                    // Calculate the target position based on camera rotation
+                    Quaternion cameraRotation = Quaternion.Euler(rotCamera);
+                    Vector3 targetPosition = transform.position + cameraRotation * _aimRigInitialOffset;
+
+                    // Update the aim target's position
+                    _aimRigTarget.position = targetPosition;
+                }
             }
         }
     }
